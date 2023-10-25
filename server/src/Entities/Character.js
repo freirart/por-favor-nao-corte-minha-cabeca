665ad52080e8896182853596716919933fbf9635
freirart <artur.santos@stone.com.br>
2023-10-25 13:12:04 -0300

    normalized name; export each char as well

--- server/src/Entities/Character.js
+++ server/src/Entities/Character.js
@@ -2,53 +2,68 @@ export class BaseCharacter {
     name = "";
     favoriteAction = "";
 
-    constructor(name, favoriteAction, catchPhrase, escalpoPhrase) {
+    constructor(
+        name,
+        favoriteAction,
+        catchPhrase,
+        escalpoPhrase,
+        normalizedName = ""
+    ) {
         this.name = name;
         this.favoriteAction = favoriteAction;
         this.catchPhrase = catchPhrase;
         this.escalpoPhrase = escalpoPhrase;
+        this.normalizedName = normalizedName
+            ? normalizedName
+            : name.toLowerCase();
     }
 }
 
+export const BOCAO = new BaseCharacter(
+    "Bocão",
+    "food",
+    "ME ESPEREM, NÃO ME DEIXEM PARA TRÁS!",
+    "ADORO CARNE FRESCA!",
+    "bocao"
+);
+
+export const FRED = new BaseCharacter(
+    "Fred",
+    "boat",
+    "CORRE GALERA, EU CUIDO DELE!",
+    "NÃO ADIANTA CORRER!"
+);
+
+export const JAIME = new BaseCharacter(
+    "Jaime",
+    "bonfire",
+    "FAÇAM FILA E ME SIGAM, CRIANÇAS!",
+    "TÁ NA HORA DE DORMIR!"
+);
+
+export const SERENA = new BaseCharacter(
+    "Serena",
+    "mountain",
+    "CALMA PESSOAL, VAI DAR TUDO CERTO.",
+    "RELAXA, NÃO VAI DOER NADA."
+);
+
+export const TATI = new BaseCharacter(
+    "Tati",
+    "tent",
+    "PRIMEIRO AS DAMAS, GENTE.",
+    "QUEM É O PRÓXIMO?"
+);
+
+export const ZECA = new BaseCharacter(
+    "Zeca",
+    "",
+    "SAIAM DA MINHA FRENTE!",
+    "NÃO TEM PARA ONDE CORRER?"
+);
+
 export class Characters {
-    static characters = [
-        new BaseCharacter(
-            "Bocão",
-            "food",
-            "ME ESPEREM, NÃO ME DEIXEM PARA TRÁS!",
-            "ADORO CARNE FRESCA!"
-        ),
-        new BaseCharacter(
-            "Fred",
-            "dock",
-            "CORRE GALERA, EU CUIDO DELE!",
-            "NÃO ADIANTA CORRER!"
-        ),
-        new BaseCharacter(
-            "Jaime",
-            "bonfire",
-            "FAÇAM FILA E ME SIGAM, CRIANÇAS!",
-            "TÁ NA HORA DE DORMIR!"
-        ),
-        new BaseCharacter(
-            "Serena",
-            "mountain",
-            "CALMA PESSOAL, VAI DAR TUDO CERTO.",
-            "RELAXA, NÃO VAI DOER NADA."
-        ),
-        new BaseCharacter(
-            "Tati",
-            "tent",
-            "PRIMEIRO AS DAMAS, GENTE.",
-            "QUEM É O PRÓXIMO?"
-        ),
-        new BaseCharacter(
-            "Zeca",
-            "",
-            "SAIAM DA MINHA FRENTE!",
-            "NÃO TEM PARA ONDE CORRER?"
-        ),
-    ];
+    static characters = [BOCAO, FRED, JAIME, SERENA, TATI, ZECA];
 
     static findByName(name) {
         return this.characters.find((character) => character.name === name);
