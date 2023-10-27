c3554fe938333e2e80cf00449f0030a0745e80af
freirart <artur.santos@stone.com.br>
2023-10-27 12:59:22 -0300

    improves choose character component

--- src/components/ChooseCharacter/index.jsx
+++ src/components/ChooseCharacter/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { Text, Box, Image, Stack } from "@chakra-ui/react";
+
 import { Characters } from "../../../server/src/Entities/Character";
 
 import IMAGES from "../../imgs/Images";
@@ -14,11 +16,11 @@ const ChooseCharacter = ({
 }) => {
   const cards = Characters.characters.map((c) => {
     const srcKey = `${c.normalizedName}Card`;
-    const alreadyInUse = game && Boolean(game.findPlayerByCharacter(c.name));
+    const disabled = game && Boolean(game.findPlayerByCharacter(c.name));
     return {
       characterName: c.name,
       src: IMAGES[srcKey],
-      alreadyInUse,
+      disabled,
     };
   });
 
@@ -40,19 +42,20 @@ const ChooseCharacter = ({
 
   return (
     <div>
-      <p>Escolha o seu personagem: </p>
-      <div className="charsWrapper">
-        {cards.map(({ src, characterName, alreadyInUse }, index) => (
-          <img
-            className={`card ${alreadyInUse ? "alreadyInUse" : ""}`}
-            key={index}
-            src={src}
-            onClick={() =>
-              alreadyInUse ? {} : setSelectedCharName(characterName)
-            }
-          />
+      <Text>Escolha o seu personagem: </Text>
+      <Stack width="951px" wrap="wrap" direction="row">
+        {cards.map(({ src, characterName, disabled }, index) => (
+          <Box className={`cardBox ${disabled ? "disabled" : ""}`} key={index}>
+            <Image
+              src={src}
+              className="card"
+              onClick={() =>
+                disabled ? {} : setSelectedCharName(characterName)
+              }
+            />
+          </Box>
         ))}
-      </div>
+      </Stack>
     </div>
   );
 };
