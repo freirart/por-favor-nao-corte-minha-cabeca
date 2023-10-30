ce907bc4aaae4e2bfaba0a10c351866ebd45fe30
freirart <artur.santos@stone.com.br>
2023-10-30 19:58:54 -0300

    choose char: improves reconnection; simplifies

--- src/components/ChooseCharacter/index.jsx
+++ src/components/ChooseCharacter/index.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import { Text, Box, Image, Stack } from "@chakra-ui/react";
+import { Text, Box, Image, Stack, Tooltip } from "@chakra-ui/react";
 
 import { Characters } from "../../../server/src/Entities/Character";
 
@@ -18,28 +18,18 @@ const ChooseCharacter = ({
   playerId,
   refreshFn,
 }) => {
-  const [selectedCharName, setSelectedCharName] = useState("");
-
-  const cards = Characters.characters.map((c) => {
-    const srcKey = `${c.normalizedName}Card`;
-    const disabled = game && Boolean(game.findPlayerByCharacter(c.name));
-    return {
-      characterName: c.name,
-      src: IMAGES[srcKey],
-      disabled,
-    };
-  });
-
   const eventName = "choose-character";
 
-  useEffect(() => {
-    if (selectedCharName && socket) {
+  const [didUserSelectItsChar, setDidUserSelectItsChar] = useState(false);
+
+  const select = (characterName) => {
+    if (socket) {
       if (!isFilledArray(socket.listeners(eventName))) {
         socket.on(eventName, (data) => {
           if (data.success) {
             mappedActions["update-players"](game, {
               playerId,
-              characterName: selectedCharName,
+              characterName,
             });
 
             console.log("> Escolhi o meu personagem!", game.summary);
@@ -51,13 +41,25 @@ const ChooseCharacter = ({
         });
       }
 
-      socket.emit(eventName, { characterName: selectedCharName });
+      socket.emit(eventName, { characterName });
 
-      externalizeCharacterName(selectedCharName);
+      externalizeCharacterName(characterName);
+      localStorage.setItem(eventName, characterName);
+      setDidUserSelectItsChar(true);
     }
-  }, [selectedCharName]);
+  };
 
-  if (!didPlayerEnteredTheGame || selectedCharName) {
+  const cards = Characters.characters.map((c) => {
+    const srcKey = `${c.normalizedName}Card`;
+    const disabled = game && Boolean(game.findPlayerByCharacter(c.name));
+    return {
+      characterName: c.name,
+      src: IMAGES[srcKey],
+      disabled,
+    };
+  });
+
+  if (!didPlayerEnteredTheGame || didUserSelectItsChar) {
     return null;
   }
 
@@ -65,17 +67,35 @@ const ChooseCharacter = ({
     <div>
       <Text>Escolha o seu personagem: </Text>
       <Stack width="951px" wrap="wrap" direction="row">
-        {cards.map(({ src, characterName, disabled }, index) => (
-          <Box className={`cardBox ${disabled ? "disabled" : ""}`} key={index}>
-            <Image
-              src={src}
-              className="card"
-              onClick={() =>
-                disabled ? {} : setSelectedCharName(characterName)
-              }
-            />
-          </Box>
-        ))}
+        {cards.map(({ src, characterName: charName, disabled }, index) => {
+          let className = "cardBox";
+
+          if (disabled) {
+            className += " disabled";
+          }
+
+          const active = localStorage.getItem(eventName) === charName;
+
+          if (active) {
+            className += " active";
+          }
+
+          return (
+            <Tooltip
+              key={index}
+              label="Este foi o seu último personagem escolhido!"
+              isDisabled={!active || disabled}
+            >
+              <Box className={className}>
+                <Image
+                  src={src}
+                  className="card"
+                  onClick={() => (disabled ? {} : select(charName))}
+                />
+              </Box>
+            </Tooltip>
+          );
+        })}
       </Stack>
     </div>
   );
