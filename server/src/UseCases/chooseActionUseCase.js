52d7e60074a1f7efcb254416251426e0ee363d4e
freirart <artur.santos@stone.com.br>
2023-11-06 21:14:22 -0300

    spaces

--- server/src/UseCases/chooseActionUseCase.js
+++ server/src/UseCases/chooseActionUseCase.js
@@ -10,48 +10,46 @@ import scoringUseCase from "./scoringUseCase.js";
  * @param {Game} game
  */
 const chooseActionUseCase = (playerId, actions, game) => {
-    if (!isFilledArray(actions)) {
-        return Error.badRequest("Actions should be an array with locations.");
+  if (!isFilledArray(actions)) {
+    return Error.badRequest("Actions should be an array with locations.");
+  }
+
+  const { characters } = Characters;
+
+  if (
+    !actions.every((a) => !a || characters.find((c) => c.favoriteAction === a))
+  ) {
+    return Error.badRequest("Actions array contain invalid actions.");
+  }
+
+  try {
+    const { currentRound, players, nextRound } = game;
+    const { currentTurn, nextTurn } = currentRound;
+    const { chosenActions, chooseAction } = currentTurn;
+
+    chooseAction(playerId, actions);
+
+    const didTurnEnd = Object.keys(chosenActions).length === players.length;
+
+    let data;
+
+    if (didTurnEnd) {
+      data = { ...chosenActions };
+
+      scoringUseCase(game);
+
+      if (currentRound.canStartANewTurn()) {
+        nextTurn();
+      } else {
+        nextRound();
+      }
     }
 
-    const { characters } = Characters;
-
-    if (
-        !actions.every(
-            (a) => !a || characters.find((c) => c.favoriteAction === a)
-        )
-    ) {
-        return Error.badRequest("Actions array contain invalid actions.");
-    }
-
-    try {
-        const { currentRound, players, nextRound } = game;
-        const { currentTurn, nextTurn } = currentRound;
-        const { chosenActions, chooseAction } = currentTurn;
-
-        chooseAction(playerId, actions);
-
-        const didTurnEnd = Object.keys(chosenActions).length === players.length;
-
-        let data;
-
-        if (didTurnEnd) {
-            data = { ...chosenActions };
-
-            scoringUseCase(game);
-
-            if (currentRound.canStartANewTurn()) {
-                nextTurn();
-            } else {
-                nextRound();
-            }
-        }
-
-        return data;
-    } catch (err) {
-        console.error(err);
-        return Error.message(err);
-    }
+    return data;
+  } catch (err) {
+    console.error(err);
+    return Error.message(err);
+  }
 };
 
 export default chooseActionUseCase;
